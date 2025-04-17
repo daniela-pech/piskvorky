d39ba8bc28d2bc65194c0f25ff3642e596b0c7f1
Daniela Pech <randakova.daniela@gmail.com>
2025-04-17 19:51:59 +0200

    zrovoznění aplikace v js - klikání na button, kolečko, křížek

---
+++ hra.js
@@ -0,0 +1,45 @@
+let currentPlayer = 'circle';
+
+const kdoHraje = document.querySelector('#hrac');
+kdoHraje.innerHTML = `
+HRAJE: <img src="images/circle.svg" alt="kolečko" class="hrac__bila">
+`;
+
+const buttonElements = document.querySelectorAll('.hra__tlacitko');
+
+buttonElements.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const button = event.target;
+    if (button.disabled) {
+      return;
+    }
+    if (currentPlayer === 'circle') {
+      button.classList.add('hra__pole--kolecko');
+    } else {
+      button.classList.add('hra__pole--krizek');
+    }
+
+    button.disabled = true;
+
+    if (currentPlayer === 'circle') {
+      currentPlayer = 'cross';
+      kdoHraje.innerHTML = `
+HRAJE: <img src="images/cross.svg" alt="křížek" class="hrac__bila">
+`;
+    } else {
+      currentPlayer = 'circle';
+      kdoHraje.innerHTML = `
+HRAJE: <img src="images/circle.svg" alt="kolečko" class="hrac__bila">
+`;
+    }
+  });
+});
+
+const restartButton = document.querySelector('.restart');
+
+restartButton.addEventListener('click', (event) => {
+  const nacistZnovu = confirm('Opravdu chceš začít znovu?');
+  if (nacistZnovu == false) {
+    event.preventDefault();
+  }
+});
