ced77e081e017139d827b01feb979f799384e1f9
Daniela Pech <randakova.daniela@gmail.com>
2025-04-30 22:09:06 +0200

    vypracovani ukolu 4, vyhodnoceni viteze, vypis viteze pres alert, reload stranjky

--- hra.js
+++ hra.js
@@ -1,5 +1,8 @@
-let currentPlayer = 'circle';
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
+let currentPlayer = 'circle'; // zacina kolecko
+
+//vpisovaci kolecko a krizek
 const kdoHraje = document.querySelector('#hrac');
 kdoHraje.innerHTML = `
 HRAJE: <img src="images/circle.svg" alt="kolečko" class="hrac__bila">
@@ -21,9 +24,40 @@ buttonElements.forEach((button) => {
       kdoHraje.innerHTML = `HRAJE: <img src="images/circle.svg" alt="kolečko" class="hrac__bila">`;
     }
     clickedButton.disabled = true;
+
+    //vyhodnoceni
+    const zjednodusenaPolicka = Array.from(buttonElements).map((policko) => {
+      if (policko.classList.contains('hra__tlacitko--krizek')) {
+        return 'x';
+      }
+      if (policko.classList.contains('hra__tlacitko--kolecko')) {
+        return 'o';
+      }
+      return '_';
+    });
+
+    const vitez = findWinner(zjednodusenaPolicka);
+
+    if (vitez === 'o') {
+      setTimeout(() => {
+        alert('Vyhrálo kolečko!');
+        location.reload();
+      }, 500);
+    } else if (vitez === 'x') {
+      setTimeout(() => {
+        alert('Vyhrál křížek!');
+        location.reload();
+      }, 500);
+    } else if (vitez === 'tie') {
+      setTimeout(() => {
+        alert('Hra skončila nerozhodně.');
+        location.reload();
+      }, 500);
+    }
   });
 });
 
+// tlacitko restart - nacist znovu nebo pokracovat
 const restartElement = document.querySelector('.restart');
 restartElement.addEventListener('click', (event) => {
   const nacistZnovu = confirm('Opravdu chceš začít znovu?');
